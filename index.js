0c129969900a1da76cb7ac4f9cbaa96c5b3469e0
Andrew Osheroff <andrewosh@gmail.com>
2016-02-11 17:05:24 -0500

    Added Binder API handling to BinderModule

--- index.js
+++ index.js
@@ -1,67 +1,134 @@
+var EventEmitter = require('events')
 var http = require('http')
 
 var _ = require('lodash')
+var inherits = require('inherits')
 var hat = require('hat')
 var express = require('express')
 var bodyParser = require('body-parser')
 var format = require('string-format')
 format.extend(String.prototype)
 
-var settings = require('./lib/settings.js')
-
+var binderProtocol = require('binder-protocol')
 var getLogger = require('binder-logging').getLogger
 
-/**
- * An HTTP server that implements API of a Binder component
- * @constructor
- */
-var BinderModule = function (options) {
-  options = options || {}
-  this.options = this._processOptions(options)
-  this.name = options.name || settings.name || 'binder-module'
-
-  this.apiKey = options.apiKey || settings.apiKey || hat()
-  this.port = options.port || settings.port
-
-  this.logger = getLogger(this.name)
-}
-
-BinderModule.prototype._processOptions = function (options) {
-  if (this.name in options) {
-    var limited = options[this.name]
+var processOptions = function (name, options) {
+  if (!options) {
+    return {}
+  }
+  if (name in options) {
+    var limited = options[name]
     limited.logging = options.logging
     limited.db = options.db
-    limited.name = this.name
-    _.forEach(_.keys(options, function (key) {
-      if (!(typeof options[key] === 'object')) {
+    limited.name = name
+    _.forEach(_.keys(options), function (key) {
+      if (!(typeof options[key] === 'object') && key !== name) {
         limited[key] = options[key]
       }
-    }))
+    })
     return limited
   }
   return options
 }
 
 /**
- * Attach all routes to the main app object
+ * An HTTP server that implements API of a Binder component
+ * @constructor
+ */
+var BinderModule = function (name, settings, options) {
+  this.name = name
+  this.opts = _.merge(settings, processOptions(this.name, options))
+  this.apiKey = this.opts.apiKey || hat()
+  this.port = this.opts.port
+
+  this.logger = getLogger(this.name)
+  if (name) {
+    this.protocol = binderProtocol[name]
+  }
+}
+inherits(BinderModule, EventEmitter)
+
+/**
+ * Declare all Binder API handlers
  *
  * Abstract method
  */
-BinderModule.prototype._makeRoutes = function (app, authHandler) {}
+BinderModule.prototype._makeBinderAPI = function (app, authHandler) {}
+
+/**
+ * Attach any other routes (that are not part of the Binder API) to the app, optionally
+ * authenticated
+ *
+ * Abstract method
+ */
+BinderModule.prototype._makeOtherRoutes = function (app, authHandler) {}
 
 BinderModule.prototype._createServer = function () {
+  var self = this
+
   var app = express()
+  app.use(bodyParser.json())
 
   var authHandler = function (req, res, next) {
     var credentials = req.headers['authorization']
-    if (credentials && credentials === this.apiKey) {
+    if (credentials && credentials === self.apiKey) {
       next()
     } else {
       res.status(403).end()
     }
   }
-
-  app.use(bodyParser.json())
+  if (this.protocol) {
+    this.logger.info('creating Binder API endpoints for the {0} API'.format(this.name))
+    var apiHandlers = this._makeBinderAPI()
+    _.forEach(apiHandlers, function (handler, name) {
+      var endpoint = self.protocol[name]
+      if (!endpoint) {
+        self.logger.error('handler trying to handle nonexistent endpoint: {0}'.format(name))
+        return
+      }
+      var basePath = endpoint.path
+      var params = _.map(endpoint.request.queryParams, function (param) {
+        return _.camelCase(param)
+      })
+      var fullPath = basePath + params.join('/')
+      var method = _.lowerCase(endpoint.request.method)
+      app[method](fullPath, function (req, res, next) {
+        var api = {}
+        var params = (method === 'get') ? req.params : req.body
+        // ensure that all required parameters are contained in the request
+        _.forEach(endpoint.params, function (type, name) {
+          if (!(name in params)) {
+            if (typeof type === 'object' && type.required === true) {
+              res.sendStatus(422)
+            }
+          }
+        })
+        // if the request made it to this point, it is a valid request
+        api.params = params
+        // attach error and success functions to the api object
+        _.forEach(endpoint.response.error, function (info, name) {
+          api['_' + name] = function (obj) {
+            var msg = obj ? info.msg.format(obj) : info.msg
+            self.logger.error(msg)
+            res.status(info.status).send({
+              // do not send implementation details in the error to the client
+              error: info.msg
+            }).end()
+          }
+        })
+        api._success = function (obj) {
+          self.logger.info(endpoint.success.msg)
+          if (!obj) {
+            res.sendStatus(endpoint.success.status)
+          } else {
+            res.status(endpoint.success.status).json(obj)
+          }
+        }
+        handler(api)
+      })
+    })
+  }
+  this.logger.info('creating any other endpoints not associated with the Binder API')
   this._makeRoutes(app, authHandler)
   return http.createServer(app)
 }
@@ -70,18 +137,25 @@ BinderModule.prototype._createServer = function () {
  * Perform any module-specific startup behaviors
  *
  * Abstract method
+ *
+ * @param {function} cb - callback(error)
  */
-BinderModule.prototype._start = function () {}
+BinderModule.prototype._start = function (cb) {}
 
 BinderModule.prototype.start = function () {
+  var self = this
   if (!this.server) {
     // Start any module-specific services
-    this._start()
-
-    this.server = this._createServer()
-    this.logger.info('Starting {0} on port {1} ...'.format(this.name, this.port))
-    this.server.listen(this.port)
-    return this.apiKey
+    this._start(function (err) {
+      if (err) {
+        self.logger.error(err)
+        process.exit(2)
+      }
+      self.server = self._createServer()
+      self.logger.info('Starting {0} on port {1} ...'.format(self.name, self.port))
+      self.emit('start')
+      self.server.listen(self.port)
+    })
   } else {
     this.logger.error('{} has already been started'.format(this.name))
   }
@@ -90,15 +164,23 @@ BinderModule.prototype.start = function () {
 /**
  * Perform any module-specific stopping behaviors
  *
- * Abstrac method
+ * Abstract method
+ *
+ * @param {function} cb - callback(error)
  */
-BinderModule.prototype._stop = function () {}
+BinderModule.prototype._stop = function (cb) {}
 
 BinderModule.prototype.stop = function () {
+  var self = this
   this.logger.info('Stopping {}...'.format(this.name))
   if (this.server) {
-    this.server.close()
-    this._stop()
+    this._stop(function (err) {
+      if (err) {
+        self.logger.error(err)
+      }
+      self.server.close()
+      self.emit('stop')
+    })
   }
 }
 
